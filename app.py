390d059a1f4a0d3ed681f84d229eade6b40aa9d5
Alejandro Fontal <alejandrofontal92@gmail.com>
2021-06-27 21:35:06 +0200

    feat: several changes

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+import io
+import base64
 import dash
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import pandas as pd
-from dash.dependencies import Output, Input, State
-from dash.exceptions import PreventUpdate
 import sdcpy.scale_dependent_correlation as sdc
-import base64
-import io
-import matplotlib.pyplot as plt
+
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Output, Input, State
 
 app = dash.Dash(
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True
 )
 GITHUB_LOGO = 'assets/github_logo_white.png'
 SIDEBAR_STYLE = {
@@ -29,18 +32,29 @@ sidebar = html.Div(
         html.A(
             dbc.Row(
                 [html.Img(src='assets/sdcpy_logo_white.png', height='140px',
-                         style={'margin-left': '15px'})],
+                          style={'margin-left': '15px'})],
                 align='center',
                 no_gutters=False,
             ),
             href='https://github.com/AlFontal/sdcpy',
         ),
-        html.Hr()], style=SIDEBAR_STYLE,
+        html.Hr(),
+        dcc.Markdown('**Source Code**', style={'color': 'white', 'margin-left': '20px'}),
+        html.A(
+            dbc.Row(
+                [html.Img(src=GITHUB_LOGO, height='60px',
+                          style={'margin-left': '50px'})],
+                align='center',
+                no_gutters=False,
+            ),
+            href='https://github.com/AlFontal/sdc-app',
+        ),
+    ], style=SIDEBAR_STYLE,
 )
-
 
 data_memory_store = dcc.Store(id='data-memory-store')
 clicks_store = dcc.Store(id='clicks-store', data=0)
+results_store = dcc.Store(id='results-store', data=None)
 
 title_row = dcc.Markdown('## Scale dependent correlation Analysis WebApp')
 
@@ -53,30 +67,29 @@ ts1_dropdown = dcc.Dropdown(options=[{'label': 'Add dataset to select', 'value':
 ts2_dropdown = dcc.Dropdown(options=[{'label': 'Add dataset to select', 'value': 'Empty'}],
                             placeholder='Select time series 2', id='ts2-dropdown')
 date_dropdown = dcc.Dropdown(options=[{'label': 'Add dataset to select', 'value': 'Empty'}],
-                            placeholder='Select time series 2', id='date-dropdown')
+                             placeholder='Select time series 2', id='date-dropdown')
 method_dropdown = dcc.Dropdown(options=[{'label': 'Spearman', 'value': 'spearman'},
                                         {'label': 'Pearson', 'value': 'pearson'}],
-                            placeholder='Select correlation method', id='method-dropdown')
+                               placeholder='Select correlation method', id='method-dropdown')
 
 parameter_rows = html.Div(
     [dcc.Markdown('Now select the parameters to run the SDC Analysis:'),
-        dbc.Row([
-        dbc.Col([html.B('Time Series 1'), ts1_dropdown], align='center'),
-        dbc.Col([html.B('Time Series 2'), ts2_dropdown], align='center'),
-        dbc.Col([html.B('Date Column'), date_dropdown], align='center'),
-], style={'width': '700px'}),
-        html.Br(),
-    dbc.Row([
-        dbc.Col([html.B('Corr. Method'), method_dropdown], align='center'),
-        dbc.Col([html.B('Min Lag'),
-                 dbc.Input(id='min-lag', placeholder='Select min lag', type='number')]),
-        dbc.Col([html.B('Max Lag'),
-                 dbc.Input(id='max-lag', placeholder='Select max lag', type='number')]),
-        dbc.Col([html.B('Window Size (s)'),
-                 dbc.Input(id='window', placeholder='Select window size', type='number', min=0)])
-    ], style={'width': '700px'})
-    ], hidden=True, id='parameters-div')
-
+     dbc.Row([
+         dbc.Col([html.B('Time Series 1'), ts1_dropdown], align='center'),
+         dbc.Col([html.B('Time Series 2'), ts2_dropdown], align='center'),
+         dbc.Col([html.B('Date Column'), date_dropdown], align='center'),
+     ], style={'width': '700px'}),
+     html.Br(),
+     dbc.Row([
+         dbc.Col([html.B('Corr. Method'), method_dropdown], align='center'),
+         dbc.Col([html.B('Min Lag'),
+                  dbc.Input(id='min-lag', placeholder='Select min lag', type='number')]),
+         dbc.Col([html.B('Max Lag'),
+                  dbc.Input(id='max-lag', placeholder='Select max lag', type='number')]),
+         dbc.Col([html.B('Window Size (s)'),
+                  dbc.Input(id='window', placeholder='Select window size', type='number', min=0)])
+     ], style={'width': '700px'})
+     ], hidden=True, id='parameters-div')
 
 file_upload = html.Div([
     dcc.Upload(
@@ -86,28 +99,28 @@ file_upload = html.Div([
             html.A('')
         ]),
         style={**{
-                'width': '500px',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px',
-                'margin-left': '2 rem'
-            }}
+            'width': '500px',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px',
+            'margin-left': '2 rem'
+        }}
     ),
     html.Div(id='output-data-upload'),
 ])
 
+results_div = html.Div(id='results-div')
+
 run_button = html.Div(dbc.Button('Run SDC Analysis',
-                      disabled=False,
-                      style={'margin': '10px'},
-                      id='run-button'),
+                                 disabled=False,
+                                 style={'margin': '10px'},
+                                 id='run-button'),
                       id='run-button-div',
                       hidden=True)
-
-results_div = html.Div(id='results-div')
 
 
 def parse_contents(contents, filename):
@@ -141,7 +154,7 @@ def update_output(content, filename):
 @app.callback([Output('ts1-dropdown', 'options'),
                Output('ts2-dropdown', 'options'),
                Output('date-dropdown', 'options')],
-               Input('data-memory-store', 'data'))
+              Input('data-memory-store', 'data'))
 def update_series(data):
     if data is not None:
         df = pd.DataFrame(data)
@@ -150,34 +163,52 @@ def update_series(data):
     else:
         raise PreventUpdate
 
+
 @app.callback([Output('results-div', 'children'),
-               Output('clicks-store', 'data')],
-               Input('run-button', 'n_clicks'),
-               Input('clicks-store', 'data'),
-               Input('ts1-dropdown', 'value'),
-               Input('ts2-dropdown', 'value'),
-               Input('date-dropdown', 'value'),
-               Input('method-dropdown', 'value'),
-               Input('min-lag', 'value'),
-               Input('max-lag', 'value'),
-               Input('window', 'value'),
-               Input('data-memory-store', 'data')
+               Output('clicks-store', 'data'),
+               ],
+              Input('run-button', 'n_clicks'),
+              Input('clicks-store', 'data'),
+              Input('ts1-dropdown', 'value'),
+              Input('ts2-dropdown', 'value'),
+              Input('date-dropdown', 'value'),
+              Input('method-dropdown', 'value'),
+              Input('min-lag', 'value'),
+              Input('max-lag', 'value'),
+              Input('window', 'value'),
+              Input('data-memory-store', 'data')
               )
 def on_run_sdc_click(n_clicks, current_clicks, ts1, ts2, date, method, min_lag, max_lag, w, data):
     if n_clicks is not None and n_clicks > current_clicks:
         df = pd.DataFrame(data).assign(date=lambda dd: pd.to_datetime(dd[date])).set_index('date')
         computed_sdc = sdc.SDCAnalysis(ts1=df[ts1], ts2=df[ts2], method=method,
-                                       min_lag=min_lag, max_lag=max_lag,
-                                       fragment_size=w)
+                                       min_lag=min_lag, max_lag=max_lag, fragment_size=w)
         buffer = io.BytesIO()
         computed_sdc.combi_plot(xlabel=ts1, ylabel=ts2, wspace=.3, hspace=.3, max_lag=max_lag,
                                 min_lag=min_lag, figsize=(6, 6), title='')
         plt.savefig(buffer, format='png', dpi=150)
         plt.close()
         data = base64.b64encode(buffer.getbuffer()).decode("utf8")
-        return [html.Img(src=f'data:image/png;base64,{data}')], n_clicks
+        image_div = html.Img(src=f'data:image/png;base64,{data}', id='sdc-results-img')
+        download_button = html.Div([
+        dbc.Button("Download Results Table", id="download-button"),
+        dcc.Download(id="download-results-xlsx")
+                ])
+        return [dcc.Markdown('### SDC Analysis Results'), download_button, image_div], n_clicks
     else:
         raise PreventUpdate
+
+
+@app.callback(
+    Output("download-results-xlsx", "data"),
+    Input("download-button", "n_clicks"),
+    Input("results-store", "data"),
+    prevent_initial_call=True,
+)
+def on_download_click(n_clicks, data):
+    if n_clicks > 0:
+        return dict(content=data, filename='test.xslx')
+
 
 content_div = html.Div([title_row,
                         html.Hr(),
@@ -188,12 +219,14 @@ content_div = html.Div([title_row,
                         run_button,
                         html.Hr(),
                         results_div],
- style={'margin-left': '10rem', 'margin-right': '2rem', 'padding': '2rem 1rem'})
+                       style={'margin-left': '10rem', 'margin-right': '2rem',
+                              'padding': '2rem 1rem'})
 
 app.layout = html.Div(children=[sidebar,
                                 content_div,
                                 data_memory_store,
-                                clicks_store])
+                                clicks_store,
+                                results_store])
 
 app.title = 'SDCpy app'
 if __name__ == '__main__':
